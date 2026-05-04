fda499b98ceba6f37f96d4c111cbbaf804875a7b
Jaime Andrés Rodriguez Gómez <jaimerodriguezgo@gmail.com>
2026-05-03 22:36:26 -0500

    arbol en proceso

--- js/script.js
+++ js/script.js
@@ -13,4 +13,194 @@ imagenOveja.src = archivoOveja[indice];
 setInterval(() => {
   indice = (indice + 1) % archivoOveja.length; // vuelve a 0 al llegar al final
   imagenOveja.src = archivoOveja[indice];
-}, 1000)
+}, 1000);
+
+const img = document.querySelector(".contenedor-arbol-genealogico img");
+const cuadroHilana = document.querySelector("#cuadro-hilana");
+const cuadroTramulo = document.querySelector("#cuadro-tramulo");
+const cuadroLucia = document.querySelector("#cuadro-lucia");
+const cuadroAugusto = document.querySelector("#cuadro-augusto");
+const cuadroJulia = document.querySelector("#cuadro-julia");
+const cuadroUrdeliano = document.querySelector("#cuadro-urdeliano");
+const cuadroMarco = document.querySelector("#cuadro-marco");
+const cuadroClaudia = document.querySelector("#cuadro-claudia");
+const cuadroPedro = document.querySelector("#cuadro-pedro");
+const cuadroLisandro = document.querySelector("#cuadro-lisandro");
+
+function actualizarCuadro(width) {
+  // hilana
+  if (width == 1000) {
+    cuadroHilana.style.top = "20px";
+    cuadroHilana.style.left = "227px";
+    cuadroHilana.style.width = "132px";
+    cuadroHilana.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroHilana.style.top = "4%";
+    cuadroHilana.style.left = "23%";
+    cuadroHilana.style.width = "13%";
+    cuadroHilana.style.height = "15%";
+  }
+
+  // tramulo
+  if (width == 1000) {
+    cuadroTramulo.style.top = "20px";
+    cuadroTramulo.style.left = "430px";
+    cuadroTramulo.style.width = "132px";
+    cuadroTramulo.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroTramulo.style.top = "4%";
+    cuadroTramulo.style.left = "43%";
+    cuadroTramulo.style.width = "13%";
+    cuadroTramulo.style.height = "15%";
+  }
+
+  // lucia
+  if (width == 1000) {
+    cuadroLucia.style.top = "156px";
+    cuadroLucia.style.left = "27px";
+    cuadroLucia.style.width = "132px";
+    cuadroLucia.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroLucia.style.top = "29%";
+    cuadroLucia.style.left = "2.5%";
+    cuadroLucia.style.width = "13%";
+    cuadroLucia.style.height = "15%";
+  }
+
+  // augusto
+  if (width == 1000) {
+    cuadroAugusto.style.top = "156px";
+    cuadroAugusto.style.left = "227px";
+    cuadroAugusto.style.width = "132px";
+    cuadroAugusto.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroAugusto.style.top = "29%";
+    cuadroAugusto.style.left = "23%";
+    cuadroAugusto.style.width = "13%";
+    cuadroAugusto.style.height = "15%";
+  }
+
+  // julia
+  if (width == 1000) {
+    cuadroJulia.style.top = "156px";
+    cuadroJulia.style.left = "429px";
+    cuadroJulia.style.width = "132px";
+    cuadroJulia.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroJulia.style.top = "29%";
+    cuadroJulia.style.left = "43%";
+    cuadroJulia.style.width = "13%";
+    cuadroJulia.style.height = "15%";
+  }
+
+  // urdeliano
+  if (width == 1000) {
+    cuadroUrdeliano.style.top = "156px";
+    cuadroUrdeliano.style.left = "633px";
+    cuadroUrdeliano.style.width = "132px";
+    cuadroUrdeliano.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroUrdeliano.style.top = "29%";
+    cuadroUrdeliano.style.left = "63.5%";
+    cuadroUrdeliano.style.width = "13%";
+    cuadroUrdeliano.style.height = "15%";
+  }
+
+  // marco
+  if (width == 1000) {
+    cuadroMarco.style.top = "290px";
+    cuadroMarco.style.left = "433px";
+    cuadroMarco.style.width = "132px";
+    cuadroMarco.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroMarco.style.top = "55%";
+    cuadroMarco.style.left = "43.2%";
+    cuadroMarco.style.width = "13%";
+    cuadroMarco.style.height = "14.5%";
+  }
+
+  // claudia
+  if (width == 1000) {
+    cuadroClaudia.style.top = "290px";
+    cuadroClaudia.style.left = "633px";
+    cuadroClaudia.style.width = "132px";
+    cuadroClaudia.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroClaudia.style.top = "55%";
+    cuadroClaudia.style.left = "63.5%";
+    cuadroClaudia.style.width = "13%";
+    cuadroClaudia.style.height = "14.5%";
+  }
+
+  // pedro
+  if (width == 1000) {
+    cuadroPedro.style.top = "290px";
+    cuadroPedro.style.left = "834px";
+    cuadroPedro.style.width = "132px";
+    cuadroPedro.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroPedro.style.top = "55%";
+    cuadroPedro.style.left = "83.6%";
+    cuadroPedro.style.width = "13%";
+    cuadroPedro.style.height = "14.5%";
+  }
+
+  // lisandro
+  if (width == 1000) {
+    cuadroLisandro.style.top = "425px";
+    cuadroLisandro.style.left = "534px";
+    cuadroLisandro.style.width = "132px";
+    cuadroLisandro.style.height = "78px";
+  } else if (width > 1000) {
+    cuadroLisandro.style.top = "80%";
+    cuadroLisandro.style.left = "53.6%";
+    cuadroLisandro.style.width = "13%";
+    cuadroLisandro.style.height = "14.5%";
+  }
+
+  /*
+      cuadroLucia.style.top = "29%";
+    cuadroLucia.style.left = "43%";
+    cuadroLucia.style.width = "13%";
+    cuadroLucia.style.height = "15%";
+  */
+}
+
+const observer = new ResizeObserver((entries) => {
+  for (let entry of entries) {
+    const width = entry.contentRect.width;
+    const height = entry.contentRect.height;
+
+    console.log("Tamaño actual:", width, height);
+    actualizarCuadro(width);
+  }
+});
+
+observer.observe(img);
+
+let infoPersonajes = document.getElementById("info-personajes");
+
+document.addEventListener('click', function(e) {
+  const cuadro = e.target.closest('.cuadro-personaje');
+
+  if (!cuadro) return;
+
+  // equivalente al console.log del primer código
+  console.log('Click en:', cuadro);
+
+  // equivalente al segundo código
+  console.log('Nombre:', cuadro.dataset.nombre);
+
+  if(cuadro.dataset.nombre == "Hilana"){
+    infoPersonajes.style.visibility = "visible";
+  }
+});
+
+let botonCerrar = document.getElementById("boton-cerrar");
+
+botonCerrar.onclick = function() {
+  infoPersonajes.style.visibility = "hidden";
+}
+
+
+
